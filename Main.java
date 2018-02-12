1b5860b4c0f50529f00de44e8bf74b646cd659c0
Ilima Turarbekova <rusilima0910@gmail.com>
2018-02-12 21:36:00 +0600

    Practice_01_Task_04

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int b = scanner.nextInt();
+
+        int summ = a + b;
+
+        System.out.printf("%d + %d = %d", a, b, summ);
+    }
+}
